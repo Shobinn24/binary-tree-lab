adfe2202187fdf55a3de8402bda4cb992ca193de
shobinn24 <shobinn24@gmail.com>
2026-02-27 12:14:09 -0500

    final resolution

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -8,8 +8,25 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    """Return the number of nodes on the longest path from root to a leaf."""
+    if root is None:
+        return 0
+
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    """Return the lowest common ancestor of p and q in a binary search tree."""
+    current = root
+
+    while current is not None:
+        if p.val < current.val and q.val < current.val:
+            current = current.left
+        elif p.val > current.val and q.val > current.val:
+            current = current.right
+        else:
+            return current
+
+    return root
